c8baa2fa19a78309541341037650725be9efd302
Kanji Miyatani <kanji09012458@gmail.com>
2024-01-10 00:30:50 +0900

    Rename member.java to user.java

--- backend/src/main/java/com/fullstack/backend/entity/Users.java
+++ backend/src/main/java/com/fullstack/backend/entity/Users.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Members {
+public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 自動生成される主キー
