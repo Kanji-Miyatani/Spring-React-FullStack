1dd80ac0d21c87c5f7ada2dd67290068b47e101b
Kanji Miyatani <kanji09012458@gmail.com>
2024-01-14 21:48:15 +0900

    Fix HostController.java

--- backend/src/main/java/com/fullstack/backend/controller/HostController.java
+++ backend/src/main/java/com/fullstack/backend/controller/HostController.java
@@ -1,9 +1,11 @@
 package com.fullstack.backend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HostController {
-     @GetMapping("/**/{path:[^.]*}")
+    @GetMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}","/error"  })
     public String any() {
         return "forward:/index.html";
     }
