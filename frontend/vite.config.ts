1cc5b19a49b01250f08689a3acf1cdd4870064ac
Kanji Miyatani <kanji09012458@gmail.com>
2024-01-14 21:48:52 +0900

    Add front pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir:"../backend/src/main/resources/static"
+    outDir:"../backend/src/main/resources/public"
   }
 })
